ef1786e451f0a5e3a402e7d3cc35b1ca4c7e13f3
Jaco <str.jac@gmail.com>
2014-11-26 13:34:44 +0200

    defined a function instead of explicit implementations

--- runga-kutta.py
+++ runga-kutta.py
@@ -21,15 +21,16 @@ h = (b - a) / n
 t = [0]
 y = [2]
 
+def f(y, t):
+    return t*y + y + t**2
 
 for k in range(0, n):
-    F_1 = t[k]*y[k] + y[k] + t[k]**2
-    F_2 = (t[k] + h/2)*(y[k] + h/2*F_1) + (y[k] + h/2*F_1) + (t[k] + h/2)**2
-    F_3 = (t[k] + h/2)*(y[k] + h/2*F_2) + (y[k] + h/2*F_2) + (t[k] + h/2)**2
-    F_4 = (t[k] + h)*(y[k] + h*F_3) + (y[k] + h*F_3) + (t[k] + h)**2
+    F_1 = f(y[k], t[k])
+    F_2 = f((y[k] + h/2*F_1), (t[k] + h/2))
+    F_3 = f((y[k] + h/2*F_2), (t[k] + h/2))
+    F_4 = f((y[k] + h*F_3), (t[k] + h))
     y.append(y[k] + h/6*(F_1 + 2*F_2 + 2*F_3 + F_4))
     t.append(t[k] + h)
-
 
 print(y[n])
 
